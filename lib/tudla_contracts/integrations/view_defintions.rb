ada618d31f66ab78050dbbec9c8c00bde0fa2a11
Tibo Mogul <tibo.mogul@gmail.com>
2026-01-15 09:35:38 +1000

    update the require path

--- lib/tudla_contracts/integrations/view_defintions.rb
+++ lib/tudla_contracts/integrations/view_defintions.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Dir.glob("view_definitions/**/*.rb").each { |file| require file }
+Dir.glob(File.join(__dir__, "view_definitions/**/*.rb")).each { |file| require file }
--- spec/tudla_contracts_spec.rb
+++ spec/tudla_contracts_spec.rb
@@ -5,3 +5,9 @@ RSpec.describe TudlaContracts do
     expect(TudlaContracts::VERSION).not_to be nil
   end
 end
+
+RSpec.describe TudlaContracts::Integrations::ViewDefinitions::ShowTabDefinition do
+  it "is defined" do
+    expect(described_class).to be_a(Class)
+  end
+end
